f9460e9c3c6b6a8aa965322f8f51c50b00cef4ea
saidamirismail <saidamirismail@gmail.com>
2024-12-21 23:09:54 +0700

    Create jawaban.java

---
+++ jawaban.java
@@ -0,0 +1,35 @@
+public class TipeDataJava {
+    public static void main(String[] args) {
+        // Tipe data byte
+        byte contohByte = 10;
+        System.out.println("Tipe data byte: " + contohByte);
+
+        // Tipe data short
+        short contohShort = 100;
+        System.out.println("Tipe data short: " + contohShort);
+
+        // Tipe data int
+        int contohInt = 1000;
+        System.out.println("Tipe data int: " + contohInt);
+
+        // Tipe data long
+        long contohLong = 100000L;
+        System.out.println("Tipe data long: " + contohLong);
+
+        // Tipe data float
+        float contohFloat = 12.34f;
+        System.out.println("Tipe data float: " + contohFloat);
+
+        // Tipe data double
+        double contohDouble = 123.456;
+        System.out.println("Tipe data double: " + contohDouble);
+
+        // Tipe data char
+        char contohChar = 'A';
+        System.out.println("Tipe data char: " + contohChar);
+
+        // Tipe data boolean
+        boolean contohBoolean = true;
+        System.out.println("Tipe data boolean: " + contohBoolean);
+    }
+}
