8ec5cffbb6b41ec3f6aabaed5ddff2f7e94ee2dc
saidamirismail <saidamirismail@gmail.com>
2024-12-21 23:11:08 +0700

    Create jawaban.py

---
+++ jawaban.py
@@ -0,0 +1,31 @@
+# Tipe data integer
+contoh_int = 10
+print("Tipe data integer:", contoh_int)
+
+# Tipe data float
+contoh_float = 12.34
+print("Tipe data float:", contoh_float)
+
+# Tipe data string
+contoh_string = "Hello, World!"
+print("Tipe data string:", contoh_string)
+
+# Tipe data boolean
+contoh_boolean = True
+print("Tipe data boolean:", contoh_boolean)
+
+# Tipe data list
+contoh_list = [1, 2, 3, 4, 5]
+print("Tipe data list:", contoh_list)
+
+# Tipe data tuple
+contoh_tuple = (1, 2, 3, 4, 5)
+print("Tipe data tuple:", contoh_tuple)
+
+# Tipe data set
+contoh_set = {1, 2, 3, 4, 5}
+print("Tipe data set:", contoh_set)
+
+# Tipe data dictionary
+contoh_dict = {"name": "Alice", "age": 25}
+print("Tipe data dictionary:", contoh_dict)
